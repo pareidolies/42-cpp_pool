663cf6748bf31fa19cceaee3de4691876260947d
pareidolies <sacha.mkara@gmail.com>
2022-11-09 21:45:27 +0100

    starting ex02

---
+++ module00/ex02/Account.cpp
@@ -0,0 +1,68 @@
+#include "Account.hpp"
+
+# include <iostream>
+
+int Account::_nbAccounts;
+int Account::_totalAmount;
+int Account::_totalNbDeposits;
+int Account::_totalNbWithdrawals;
+
+Account::Account(int initial_deposit)
+{
+	
+}
+
+Account::~Account()
+{
+
+}
+
+int	getNbAccounts( void )
+{
+
+}
+
+int	getTotalAmount( void )
+{
+
+}
+
+int	getNbDeposits( void )
+{
+
+}
+
+int	getNbWithdrawals( void )
+{
+
+}
+
+void	displayAccountsInfos( void )
+{
+
+}
+
+void	makeDeposit( int deposit )
+{
+
+}
+
+bool	makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		checkAmount( void ) const
+{
+
+}
+
+void	displayStatus( void ) const
+{
+
+}
+
+static void	_displayTimestamp( void )
+{
+
+}
---
+++ module00/ex02/tests.d
@@ -0,0 +1,1 @@
+tests.o: tests.cpp Account.hpp
