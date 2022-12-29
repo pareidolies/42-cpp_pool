ab9ee361b44a0f61d96ea9dde52293b3af7d2c1d
pareidolies <sacha.mkara@gmail.com>
2022-12-29 20:20:42 +0100

    still need to fix the printing

---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <math.h>
+# include <cmath>
+# include <string>
+
+# define ANSI_PURPLE		"\x1b[95m"
+# define ANSI_BLUE		"\x1b[94m"
+# define ANSI_YELLOW		"\x1b[93m"
+# define ANSI_RESET			"\x1b[0m"
+
+class Fixed
+{
+	public:
+
+		Fixed(void); //default constructor
+		Fixed(Fixed const & copy); // constructeur de recopie
+		Fixed	&operator=(Fixed const & rhs); // surcharge de l'opérateur d'affectation
+		~Fixed(void); //destructeur
+		Fixed(int const nbr); //int constructor
+		Fixed(float const nbr); //float constructor
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+		float toFloat( void ) const;
+		int toInt( void ) const;
+
+	private:
+		
+		int					_fixedPointNbr;
+		static const int	_fractBitsNbr = 8;
+
+};
+
+std::ostream &operator<<(std::ostream & out, Fixed const & copy); //surcharge de l'opérateur d'insertion
+
+#endif
