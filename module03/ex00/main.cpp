df0cfae7305facbead990db93d76bd593119f313
pareidolies <sacha.mkara@gmail.com>
2023-01-19 13:16:14 +0100

    changes in main

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -13,5 +13,15 @@ int main()
 	labradoodle.attack("Woolf");
 	labradoodle.beRepaired(1);
     woolf.attack("Labradoodle");
+
+    ClapTrap	fox = ClapTrap("Fox");
+	ClapTrap	mouse = ClapTrap("Mouse");
+
+    fox.attack("Mouse");
+    mouse.takeDamage(2);
+    mouse.beRepaired(2);
+    mouse.attack("fox");
+    fox.takeDamage(1);
+
 	return (0);
 }
