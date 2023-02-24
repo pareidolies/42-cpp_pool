f6c09825371d709e81d2ca8bc7299ae54d63192e
pareidolies <sacha.mkara@gmail.com>
2023-02-24 17:36:56 +0100

    little changes

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -2,26 +2,29 @@
 
 int main()
 {
-	ClapTrap	woolf = ClapTrap("Woolf");
-	ClapTrap	labradoodle = ClapTrap();
-
-	for (int i = 0; i < 10; i++)
 	{
-		woolf.attack("Labradoodle");
-		labradoodle.takeDamage(1);
+		ClapTrap	woolf = ClapTrap("Woolf");
+		ClapTrap	labradoodle = ClapTrap();
+
+		for (int i = 0; i < 10; i++)
+		{
+			woolf.attack("Labradoodle");
+			labradoodle.takeDamage(1);
+		}
+		labradoodle.attack("Woolf");
+		labradoodle.beRepaired(1);
+	    woolf.attack("Labradoodle");
 	}
-	labradoodle.attack("Woolf");
-	labradoodle.beRepaired(1);
-    woolf.attack("Labradoodle");
 
-    ClapTrap	fox = ClapTrap("Fox");
-	ClapTrap	mouse = ClapTrap("Mouse");
+	{
+		ClapTrap	fox = ClapTrap("Fox");
+		ClapTrap	mouse = ClapTrap("Mouse");
 
-    fox.attack("Mouse");
-    mouse.takeDamage(2);
-    mouse.beRepaired(2);
-    mouse.attack("fox");
-    fox.takeDamage(1);
-
+	    fox.attack("Mouse");
+	    mouse.takeDamage(9);
+	    mouse.beRepaired(9);
+	    mouse.attack("fox");
+	    fox.takeDamage(1);
+	}
 	return (0);
 }
