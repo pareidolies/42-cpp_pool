1f8578a7468435c544214dec1d7b5bf41ba8236e
pareidolies <sacha.mkara@gmail.com>
2023-03-11 17:41:17 +0100

    module done

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -1,7 +1,9 @@
-#ifndef ITER_HPP
-# define ITER_HPP
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
 # include <stddef.h>
+# include <iostream>
+# include <exception>
 
 # define ANSI_PURPLE		"\x1b[95m"
 # define ANSI_BLUE			"\x1b[94m"
@@ -10,13 +12,70 @@
 # define ANSI_GREEN			"\x1b[92m"
 # define ANSI_RESET			"\x1b[0m"
 
-template <typename T>
-void iter(T tab[], size_t size, void (*f)(T &))
+template<typename T>
+class Array
 {
-	if (!tab || !f)
-		return ;
-	for (size_t i = 0; i < size; i++)
-		f(tab[i]);
-}
+	public:
+
+		class	OutOfBounds : public std::exception
+		{
+			public:
+
+				virtual char const * what(void) const throw()
+				{
+					return ("Exception: Array out of bounds");
+				}
+		};
+
+		Array(void) :  _size(0), _array(NULL) {} //default constructor
+		Array(unsigned int n) :  _size(n), _array(new T[n]) {} //unsigned int constructor
+		Array(Array<T> const & copy) : _size(copy._size), _array(new T[copy._size]) //copy constructor
+		{
+			for (unsigned int i = 0; i < _size; i++)
+				_array[i] = copy._array[i];
+		}
+		~Array(void) //destructor
+		{
+			if (_array)
+				delete [] _array;
+		}
+
+		Array	&operator=(Array<T> const & rhs) //assignement operator
+		{
+			if (this != &rhs) {
+				_size = rhs._size;
+				if (_array)
+					delete [] _array;
+				_array = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					_array[i] = rhs[i];
+			}
+			return (*this);
+		}
+
+		T &	operator[](unsigned int n)
+		{
+			if (n >= _size)
+				throw (OutOfBounds());
+			return (this->_array[n]);
+		}
+		T const &	operator[](unsigned int n) const 
+		{
+			if (n >= _size)
+				throw (OutOfBounds());
+			return (this->_array[n]);
+		}
+
+		unsigned int	size(void) const
+		{
+			return (this->_size);
+		}
+
+	private:
+
+		unsigned int	_size;
+		T *				_array;
+
+};
 
 #endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,46 +1,155 @@
-# include "iter.hpp"
+# include "Array.hpp"
 
 # include <iostream>
 # include <stddef.h>
 
-template <typename T>
-void ft_print(T & current)
+# define MAX_VAL 750
+
+int main(void)
 {
-    std::cout << current << " ";
-}
+	//My own main.c
+	{
+		std::cout << ANSI_BLUE << "-My own main.c-" << ANSI_RESET << std::endl << std::endl;
 
-template <typename T>
-void ft_increment(T & current)
-{
-    current++;
-}
+		Array<int> empty;
+		Array<int> fibonacci(9);
+		Array<int> fortytwo(2);
 
-int main()
-{
-	int		i[] = {0, 1, 1, 2, 3, 5, 8, 13, 21};
-	float	f[] = {21.21f, 42.42f, 84.84f};
-	char	s[] = "Hello everyone!";
+		std::cout << "Trying to print empty array:" << std::endl;
 
-	iter<int>(i, sizeof(i) / sizeof(int), &ft_print);
-	iter<int>(i, sizeof(i) / sizeof(int), &ft_increment);
-	std::cout << std::endl;
-	iter<int>(i, sizeof(i) / sizeof(int), &ft_print);
-	
-	std::cout << std::endl;
+		try 
+		{
+			std::cout << empty[0] << std::endl;
+		} 
+		catch (std::exception & e) 
+		{
+			std::cerr << e.what() << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		fibonacci[0] = 0;
+		fibonacci[1] = 1;
+		for (unsigned int i = 2; i < fibonacci.size(); i++)
+			fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+		std::cout << "Fibonacci array:" << std::endl;
+		for (unsigned int i = 0; i < fibonacci.size(); i++)
+			std::cout << fibonacci[i] << " ";
+		
+		std::cout << std::endl;
+
+		std::cout << "Size: " << fibonacci.size() << std::endl;
+
+		fortytwo[0] = 21;
+		fortytwo[1] = 42;
+		fibonacci = fortytwo;
+
+		std::cout << std::endl;
+
+		std::cout << "Fortytwo array:" << std::endl;
+		for (unsigned int i = 0; i < fortytwo.size(); i++)
+			std::cout << fortytwo[i] << " ";
+
+		std::cout << std::endl;
+		
+		std::cout << "Size: " << fortytwo.size() << std::endl;
+		
+		std::cout << std::endl;
+
+		std::cout << "Fibonacci array becomes fortytwo array:" << std::endl;
+		for (unsigned int i = 0; i < fibonacci.size(); i++)
+			std::cout << fibonacci[i] << " ";
+		
+		std::cout << std::endl;
+		std::cout << "Size: " << fibonacci.size() << std::endl;
+		std::cout << std::endl;
+
+		std::cout << "Try & catch:" << std::endl;
+		try 
+		{
+			fibonacci[0] = 0;
+			std::cout << fibonacci[0] << std::endl;
+			fibonacci[1] = 1;
+			std::cout << fibonacci[1] << std::endl;
+			fibonacci[2] = 1;
+			std::cout << fibonacci[2] << std::endl;
+		} 
+		catch (std::exception & e) 
+		{
+			std::cerr << e.what() << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		std::cout << "New fibonacci array:" << std::endl;
+		for (unsigned int i = 0; i < fibonacci.size(); i++)
+			std::cout << fibonacci[i] << " ";
+
+		std::cout << std::endl;
+		std::cout << "Size: " << fibonacci.size() << std::endl;
+		std::cout << std::endl;
+
+		std::cout << "Fortytwo array (did it change?):" << std::endl;
+		for (unsigned int i = 0; i < fortytwo.size(); i++)
+			std::cout << fortytwo[i] << " ";
+
+		std::cout << std::endl;
+		std::cout << "Size: " << fortytwo.size() << std::endl;
+		std::cout << std::endl;
+	}
+
 	std::cout << std::endl;
 
-	iter<float>(f, sizeof(f) / sizeof(float), &ft_print);
-	iter<float>(f, sizeof(f) / sizeof(float), &ft_increment);
-	std::cout << std::endl;
-	iter<float>(f, sizeof(f) / sizeof(float), &ft_print);
-	
-	std::cout << std::endl;
-	std::cout << std::endl;
+	//Main.c provided by 42
+	{
+		std::cout << ANSI_BLUE << "-Main.c provided by 42-" << ANSI_RESET << std::endl << std::endl;
 
-	iter<char>(s, sizeof(s), ft_print);
-	iter<char>(s, sizeof(s), ft_increment);
-	std::cout << std::endl;
-	iter<char>(s, sizeof(s), ft_print);
-	
-	return 0;
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;//
+	}
+	return (0);
 }
