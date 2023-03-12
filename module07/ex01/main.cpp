a34ba889c20c8cc8f828c529a78eccdeabf11fc8
pareidolies <sacha.mkara@gmail.com>
2023-03-12 17:14:55 +0100

    adding const

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -4,15 +4,21 @@
 # include <stddef.h>
 
 template <typename T>
-void ft_print(T & current)
+void ft_print(T const & current)
 {
     std::cout << current << " ";
 }
 
 template <typename T>
-void ft_increment(T & current)
+void ft_increment(T const & current)
 {
-    current++;
+    std::cout << current + 1 << " ";;
+}
+
+template <typename T>
+void ft_increment_char(T const & current)
+{
+    std::cout << (char)(current + 1) << " ";;
 }
 
 int main()
@@ -22,25 +28,41 @@ int main()
 	char	s[] = "Hello everyone!";
 
 	iter<int>(i, sizeof(i) / sizeof(int), &ft_print);
+	std::cout << std::endl;
 	iter<int>(i, sizeof(i) / sizeof(int), &ft_increment);
-	std::cout << std::endl;
-	iter<int>(i, sizeof(i) / sizeof(int), &ft_print);
 	
 	std::cout << std::endl;
 	std::cout << std::endl;
 
 	iter<float>(f, sizeof(f) / sizeof(float), &ft_print);
+	std::cout << std::endl;
 	iter<float>(f, sizeof(f) / sizeof(float), &ft_increment);
-	std::cout << std::endl;
-	iter<float>(f, sizeof(f) / sizeof(float), &ft_print);
 	
 	std::cout << std::endl;
 	std::cout << std::endl;
 
 	iter<char>(s, sizeof(s), ft_print);
-	iter<char>(s, sizeof(s), ft_increment);
 	std::cout << std::endl;
-	iter<char>(s, sizeof(s), ft_print);
-	
+	iter<char>(s, sizeof(s), ft_increment_char);
+
 	return 0;
 }
+
+/*class Awesome
+{
+public:
+Awesome( void ) : _n( 42 ) { return; }
+int get( void ) const { return this->_n; }
+private:
+int _n;
+};
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+int main() {
+int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+Awesome tab2[5];
+iter( tab, 5, print );
+iter( tab2, 5, print );
+return 0;
+}*/
